123b47a07b36ae0912c137662391cc4b3681bd88
Rafał Wojdyła <omeg@invisiblethingslab.com>
2020-07-08 18:22:25 +0200

    fixup! Rewrite image builder in Rust

--- gvmkit/src/rwbuf.rs
+++ gvmkit/src/rwbuf.rs
@@ -1,6 +1,5 @@
-use anyhow::Result;
 use bytes::{Buf, Bytes, BytesMut};
-use std::io::{Read, Write};
+use std::io::{Read, Result, Write};
 
 // wrapper for BytesMut so we can implement Read/Write
 pub struct RWBuffer {
@@ -24,18 +23,18 @@ impl RWBuffer {
 }
 
 impl Write for RWBuffer {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
         self.bytes.extend_from_slice(buf);
         Ok(buf.len())
     }
 
-    fn flush(&mut self) -> std::io::Result<()> {
+    fn flush(&mut self) -> Result<()> {
         Ok(())
     }
 }
 
 impl Read for RWBuffer {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         self.bytes.copy_to_slice(buf);
         Ok(buf.len())
     }
