92b0cc454b45ef61cdbf0de38de2d2ee536f8f70
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-06-05 19:54:40 +0200

    Offer template with VM self test info. Dependencies update.

--- runtime/src/self_test.rs
+++ runtime/src/self_test.rs
@@ -1,10 +1,11 @@
+use anyhow::bail;
 use futures::lock::Mutex;
-use serde_json::Value;
+use std::path::PathBuf;
 use std::sync::{mpsc, Arc};
 use tokio::fs;
 use ya_runtime_sdk::runtime_api::server::RuntimeHandler;
 use ya_runtime_sdk::{runtime_api::server, server::Server, Context, ErrorExt, EventEmitter};
-use ya_runtime_sdk::{ProcessStatus, RuntimeStatus};
+use ya_runtime_sdk::{Error, ProcessStatus, RuntimeStatus};
 
 use crate::deploy::Deployment;
 use crate::vmrt::{runtime_dir, RuntimeData};
@@ -12,31 +13,43 @@ use crate::Runtime;
 
 const FILE_TEST_IMAGE: &'static str = "self-test.gvmi";
 
-pub(crate) async fn test() -> anyhow::Result<()> {
+pub(crate) async fn test() -> Result<(), Error> {
+    run_self_test(verify_status).await;
+    // Dead code. ya_runtime_api::server::run_async requires killing a process to stop
+    Ok(())
+}
+
+pub(crate) fn verify_status(status: anyhow::Result<ProcessStatus>) -> anyhow::Result<String> {
+    let Ok(status) = status else {
+        bail!("Failed to get self test status: {err}");
+    };
+    if status.return_code == 0 {
+        return Ok(String::from_utf8(status.stdout)?);
+    }
+    match String::from_utf8(status.stderr) {
+        Ok(stderr) => anyhow::bail!(
+            "Process failed, code: {}, stderr: {stderr}",
+            status.return_code
+        ),
+        Err(err) => {
+            anyhow::bail!(
+                "Process failed, code: {}. Failed to parse err output: {err}",
+                status.return_code
+            )
+        }
+    }
+}
+
+pub(crate) async fn run_self_test<HANDLER>(handle_result: HANDLER)
+where
+    HANDLER: Fn(anyhow::Result<ProcessStatus>) -> anyhow::Result<String>,
+{
     let work_dir = std::env::temp_dir();
 
-    let package_path = runtime_dir()
-        .expect("Runtime directory not found")
-        .join(FILE_TEST_IMAGE)
-        .canonicalize()
-        .expect("Test image not found");
-
-    log::info!("Task package: {}", package_path.display());
-
-    let package_file = fs::File::open(package_path.clone())
+    let deployment = self_test_deployment(&work_dir)
         .await
-        .or_err("Error reading package file")?;
-    let deployment = Deployment::try_from_input(package_file, 1, 125, package_path.clone())
-        .await
-        .or_err("Error reading package metadata")?;
+        .expect("Prepares self test img deployment");
 
-    for vol in &deployment.volumes {
-        let vol_dir = work_dir.join(&vol.name);
-        log::debug!("Creating volume dir: {vol_dir:?} for path {}", vol.path);
-        fs::create_dir_all(vol_dir)
-            .await
-            .or_err("Failed to create volume dir")?;
-    }
     let runtime_data = RuntimeData {
         deployment: Some(deployment),
         ..Default::default()
@@ -45,11 +58,10 @@ pub(crate) async fn test() -> anyhow::Result<()> {
         data: Arc::new(Mutex::new(runtime_data)),
     };
 
-    let mut status: Option<ProcessStatus> = None;
     server::run_async(|e| async {
-        let ctx = Context::try_new().expect("Failed to initialize context");
+        let ctx = Context::try_new().expect("Creates runtime context");
 
-        log::info!("Starting");
+        log::info!("Starting runtime");
         let (status_sender, mut status_receiver) = mpsc::channel();
         let emitter = EventEmitter::spawn(ProcessOutputHandler {
             handler: Box::new(e),
@@ -57,60 +69,75 @@ pub(crate) async fn test() -> anyhow::Result<()> {
         });
         let start_response = crate::start(work_dir.clone(), runtime.data.clone(), emitter.clone())
             .await
-            .expect("Failed to start runtime");
-        log::info!("Response {:?}", start_response);
+            .expect("Starts runtime");
+        log::info!("Runtime start response {:?}", start_response);
 
-        let run: ya_runtime_sdk::RunProcess = server::RunProcess {
+        let run_process: ya_runtime_sdk::RunProcess = server::RunProcess {
             bin: "/ya-self-test".into(),
             args: vec!["ya-self-test".into()],
-            work_dir: "/data".into(),
+            work_dir: "/".into(),
             ..Default::default()
         };
 
-        log::debug!("Starting. Runtime: {:?}", runtime.data);
-        log::debug!("Run: {run:?}");
+        log::info!("Runtime: {:?}", runtime.data);
+        log::info!("Self test process: {run_process:?}");
 
-        let pid = crate::run_command(runtime.data.clone(), run)
+        let pid: u64 = crate::run_command(runtime.data.clone(), run_process)
             .await
-            .expect("Can run command");
+            .expect("Runs command");
 
         let (final_status_sender, final_status_receiver) = tokio::sync::oneshot::channel();
         tokio::spawn(async move {
-            let status = async {
-                listen_process_status(&mut status_receiver, pid).expect("Can listen on process")
-            }
-            .await;
+            let status = collect_process_status(&mut status_receiver, pid).await;
             final_status_sender.send(status)
         });
-        status = Some(final_status_receiver.await.expect("Got status"));
-        log::info!("Process finished: {status:?}");
+        let process_result = final_status_receiver
+            .await
+            .expect("Receives process status");
+
+        log::info!("Process finished");
+        let result = handle_result(process_result).expect("Handles test result");
+        if !result.is_empty() {
+            println!("{result}");
+        }
 
         log::info!("Stopping runtime");
         crate::stop(runtime.data.clone())
             .await
-            .expect("Failed to stop runtime");
+            .expect("Stops runtime");
 
         tokio::spawn(async move {
             // the server refuses to stop by itself; force quit
             std::process::exit(0);
         });
 
-        let status = status.expect("Failed to run self test process");
-        if status.return_code == 0 {
-            let response: Value = serde_json::from_slice(&status.stdout)
-                .expect("Cannot serialize self test stdout to json.");
-            println!("{}", response.to_string())
-        } else {
-            let err_message = std::str::from_utf8(&status.stderr)
-                .expect("Can read stderr as string.")
-                .to_string();
-            eprintln!("self test process failed: {err_message}");
-        }
-
         Server::new(runtime, ctx)
     })
     .await;
-    Ok(())
+}
+
+async fn self_test_deployment(work_dir: &PathBuf) -> anyhow::Result<Deployment> {
+    let package_path = runtime_dir()
+        .expect("Runtime directory not found")
+        .join(FILE_TEST_IMAGE)
+        .canonicalize()
+        .expect("Test image not found");
+
+    log::info!("Task package: {}", package_path.display());
+    let package_file = fs::File::open(package_path.clone())
+        .await
+        .or_err("Error reading package file")?;
+    let deployment = Deployment::try_from_input(package_file, 1, 125, package_path.clone())
+        .await
+        .or_err("Error reading package metadata")?;
+    for vol in &deployment.volumes {
+        let vol_dir = work_dir.join(&vol.name);
+        log::debug!("Creating volume dir: {vol_dir:?} for path {}", vol.path);
+        fs::create_dir_all(vol_dir)
+            .await
+            .or_err("Failed to create volume dir")?;
+    }
+    Ok(deployment)
 }
 
 struct ProcessOutputHandler {
@@ -121,7 +148,7 @@ struct ProcessOutputHandler {
 impl RuntimeHandler for ProcessOutputHandler {
     fn on_process_status<'a>(&self, status: ProcessStatus) -> futures::future::BoxFuture<'a, ()> {
         if let Err(err) = self.status_sender.send(status.clone()) {
-            log::error!("Failed to send process status {err}");
+            log::warn!("Failed to send process status {err}");
         }
         self.handler.on_process_status(status)
     }
@@ -131,7 +158,7 @@ impl RuntimeHandler for ProcessOutputHandler {
     }
 }
 
-fn listen_process_status(
+async fn collect_process_status(
     status_receiver: &mut mpsc::Receiver<ProcessStatus>,
     pid: u64,
 ) -> anyhow::Result<ProcessStatus> {
@@ -150,10 +177,9 @@ fn listen_process_status(
             break;
         }
     }
-    let running = false;
     Ok(ProcessStatus {
         pid,
-        running,
+        running: false,
         return_code,
         stdout,
         stderr,
