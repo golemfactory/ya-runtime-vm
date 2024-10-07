b37d173b0e622407ab17741d2196889ba175217d
Jakub Leszczak <jakub.leszczak@golem.network>
2024-10-07 12:28:08 +0200

    Run `make libseccomp.a` in build.rs to make `cargo build` work

--- rinit/build.rs
+++ rinit/build.rs
@@ -1,3 +1,9 @@
+use std::process::Command;
+
 fn main() {
     println!("cargo:rustc-link-search={}/extern-libs", env!("CARGO_MANIFEST_DIR"));
+    let status = Command::new("make").args(&["extern-libs/libseccomp.a"]).status().unwrap();
+    if !status.success() {
+        panic!("Error building libseccomp.a library");
+    }
 }
