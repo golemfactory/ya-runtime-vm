5aa6a2adfb2c9e7e5ed83e7263fd8785ce10fa56
Jakub Leszczak <jakub.leszczak@golem.network>
2024-10-15 10:10:56 +0200

    Update ya-runtime-vm.json version field automaticaly

--- runtime/build.rs
+++ runtime/build.rs
@@ -1,18 +1,19 @@
 use anyhow::Context;
+use serde_json::{Map, Value};
 use std::env;
+use std::fs::File;
 use std::ops::Not;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 static RERUN_IF_CHANGED: &str = "cargo:rerun-if-changed";
 
-fn main() -> anyhow::Result<()> {
+fn make_init(root_dir: &Path) -> anyhow::Result<()> {
     // skip build for CI
     if env::var("CI").is_ok() {
         return Ok(());
     }
 
-    let root_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let init_dir = root_dir.join("init-container").canonicalize().unwrap();
     let include_dir = init_dir.join("include");
     let src_dir = init_dir.join("src");
@@ -50,5 +51,26 @@ fn main() -> anyhow::Result<()> {
         include = include_dir.display(),
         src = src_dir.display(),
     );
+    Ok(())
+}
+
+fn update_conf(root_dir: &Path, version: String) {
+    let fname = root_dir.join("conf/ya-runtime-vm.json.in");
+    let fin = File::open(fname).unwrap();
+    let fname = root_dir.join("conf/ya-runtime-vm.json");
+    let fout = File::create(fname).unwrap();
+
+    let mut json: Vec<Map<String, Value>> = serde_json::from_reader(fin).unwrap();
+    json[0].insert(String::from("version"), Value::from(version));
+    serde_json::to_writer_pretty(fout, &json).unwrap();
+}
+
+fn main() -> anyhow::Result<()> {
+    let root_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let version = env::var("CARGO_PKG_VERSION").unwrap();
+
+    make_init(&root_dir)?;
+    update_conf(&root_dir, version);
+
     Ok(())
 }
